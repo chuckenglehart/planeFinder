07bae9569b3b5775d568ab6b52de525c35aa9b09
chuckenglehart <chuck07@gmail.com>
2017-12-14 16:25:20 -0500

    Addid capability to write dta to a subfolder

--- planeFinder.py
+++ planeFinder.py
@@ -1,6 +1,9 @@
 import sys
 import pyModeS as pms
 import socket
+#import calendar
+import time
+
 
 #TODO: Figure out how defs work
 #TODO: Figure a better way to do this
@@ -42,6 +45,8 @@ ip_addr = ""
 port = 30002
 skip = 0
 desiredIC = ()
+to_write = 0
+
 
 #read in arguments 
 for x in range(0,num_args):
@@ -112,46 +117,66 @@ while True:
 	line = getLine(s)
 	#there is a star in the beginning and a semi colon at the endS. 
 	#could look into why more.
+	
+	#get the icao
 	ic = pms.adsb.icao(line)
 	if ic not in desiredIC:
-		continue#pass
+		pass#continue#pass
 	
 	#Open the IC file
+	# TODO: Better way to write in other places
+	ic_file="data/"+ic+".txt"
+	ic_f = open(ic_file, "a")
+		
+	# Initialize the lineout
+	lineout = (ic_file)	
 	
 	df = pms.adsb.df(line)
 	tc = pms.adsb.typecode(line)
 	#whats the additional capabilities?
 	
-	if (df == 17 or df == 18) and tc >= 1 and tc <= 4:
-		#identifier
+	if (df == 17 or df == 18) and tc >= 1 and tc <= 4: #identifier
 		id = pms.adsb.callsign(line)
-		print "ID of ic:",ic," is:",id
-	elif (df == 17 or df == 18) and tc >= 5 and tc <= 8:
-		#surface positions
+		lineout = "IC:"+str(ic)+" is:"+str(id)
+		to_write = 1
+	elif (df == 17 or df == 18) and tc >= 5 and tc <= 8: #surface positions
 		pass
-	elif (df == 17 or df == 18) and tc >= 9 and tc <= 18:
-		#airborne positions - "position message"
+	elif (df == 17 or df == 18) and tc >= 9 and tc <= 18: #airborne positions - "position message"
 		#do with only one message since we have the reference location
 		(lat,lon) = pms.adsb.airborne_position_with_ref(line, ref_lat, ref_lon)
 		alt = pms.adsb.altitude(line)
-		print "IC:",ic,"is at",lat,",",lon,"and",alt,"feet high"
-	elif (df == 17 or df == 18) and tc == 19:
-		#airborne velocity
+		lineout = "IC:"+str(ic)+" is at:"+str(lat)+","+str(lon)+" and altitude:"+str(alt)+" ft."
+		to_write = 1
+	elif (df == 17 or df == 18) and tc == 19: #airborne velocity
 		#s_head = pms.adsb.speed_heading(line)
 		#s_v = pms.adsb.surface_velocity(line)
 		vel = pms.adsb.velocity(line)
-		print "IC:",ic," heading:",vel[1],"at:",vel[0],"kt climbing:",vel[2],"ft/min.",vel[3]
-	elif (df == 17 or df == 18) and tc >= 20 and tc <= 22:
-		#Airborne position (w/ GNSS Height)	
+		lineout="IC:"+str(ic)+" heading:"+str(vel[1])+" vel:"+str(vel[0])+" kt climbing:"+str(vel[2])+" ft/min. "+str(vel[3])
+		to_write = 1
+	elif (df == 17 or df == 18) and tc >= 20 and tc <= 22: #Airborne position (w/ GNSS Height)	
 		pass
-	elif (df == 17 or df == 18) and tc >= 23 and tc <= 31:
-		#reserved	
+	elif (df == 17 or df == 18) and tc >= 23 and tc <= 31: #reserved	
 		pass
-	else:
+	else: #This should never be gotten to 
 		pass
-		#print "Unknown TC:",tc," or df:",df,"in line:",line
+		#lineout = (print "Unknown TC:",tc," or df:",df,"in line:",line)
 
+	timeout = time.strftime("%Y%m%dT%H:%M:%S",time.gmtime())
+	lineout=timeout+" "+lineout
+	sout = str(lineout)
 	
+	# Always print output
+	# Need to fix this up to make it more operational
+	if to_write:	
+		print sout
+		
+	# If print to file
+	if to_write:
+		ic_f.write(sout)
+		ic_f.write("\n")#to make a newline. Done automatically in print
+		
+	ic_f.close()
+	to_write = 0
 	#log data if it matches the icao in list
 
 
