260468e0deb032a7d905ff7a20aae1825db8fbcb
chuckenglehart <chuck07@gmail.com>
2018-03-06 15:01:59 -0500

    Formatting fixes for pylint

--- planePlotter.py
+++ planePlotter.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """ Written to target Python3
-Requires gmplot to be installed. 
+Requires gmplot to be installed.
 """
 
+import argparse
+from pprint import pprint # for pretty dictionary printing.
+import gmplot
 import planeUtil as pUtil
 import planeDatabase as pd
-import argparse
-import gmplot
-from pprint import pprint # for pretty dictionary printing. 
 
-def create_dict_from_db(icao, start=-1, end=-1):
-    #connect to db
-
-    #get cursor
-
-    #use get_dict function
-    pass
-
-if __name__ == '__main__':    
-
-    """Create the command line argument parser. This is also used to 
-    give the help functionality without having to build it manually. 
-    TODO: Figure out why the required arguments show up under optional. 
+def main():
+    """Create the command line argument parser. This is also used to
+    give the help functionality without having to build it manually.
+    TODO: Figure out why the required arguments show up under optional.
     """
     parser = argparse.ArgumentParser(description='Create a map from data file.')
     parser.add_argument('-i', '--icao', nargs=1, required=True, help='ICAO number to create a map from')
@@ -32,11 +23,14 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
     args = parser.parse_args()
     print(args)
-        
+
     debug = args.debug
-    
-    icao = args.icao[0]        
-    dbc = pd.dbConnection() 
+    icao = args.icao[0]
+    dbc = pd.DBConnection()
+
+    ref_lon = -76.600144
+    ref_lat = 39.070902
+
     if dbc.get_connection_isopen():
         print("Database connection open")
     else:
@@ -44,46 +38,44 @@ if __name__ == '__main__':
         print("Database connection not open")
 
     #filename="data/"+icao+".txt"
-    
+
     try:
         #get the name from an io txt wrapper
         output_filename = args.output.name[0]
-    except: 
+    except:
         output_filename = icao+".html"
 
     print('Output is :', output_filename)
 
     #Create the data storage
-    dict=dbc.get_dict(icao)
+    data_dict = dbc.get_dict(icao)
     #dataList=dbc.get_dict(icao)
-    pprint(dict)
-    
+    pprint(data_dict)
+
     #check to see if there is a start and end
     try:
         start = args.start[0]
         if debug:
-            print('Start is:',args.start[0])
+            print('Start is:', args.start[0])
     except:
         start = -1
         if debug:
             print('No start')
-    
+
     try:
         end = args.end[0]
         if debug:
-            print('end is:',args.end[0])
+            print('end is:', args.end[0])
     except:
         end = -1
         if debug:
             print('No end')
 
-    
-    
     #Now we have the dictionary of what we want to plot. So plot it.
     lats = []
     lons = []
-    gmap = gmplot.GoogleMapPlotter(39.0709, -76.60024, 8)
-    
+    gmap = gmplot.GoogleMapPlotter(ref_lat, ref_lon, 8)
+
     """
     for row in dataList:
         pprint(row['time'])
@@ -101,9 +93,9 @@ if __name__ == '__main__':
     """
     #need to rewrite the get dict to make this work
     #temportarily using the above to amek a list work
-    
-    d_keys = iter(dict.keys()) 
-    
+
+    d_keys = iter(data_dict.keys())
+
     for key in d_keys:
         if start != -1:
             if key < start:
@@ -111,17 +103,19 @@ if __name__ == '__main__':
         if end != -1:
             if key > end:
                 continue
- 
-        td = dict[key]
+
+        td = data_dict[key]
         if 'lat' in td and type(td['lat']) is float:
             lats.append(float(td['lat']))
             lons.append(float(td['lon']))
             #print(float(td['lat']),",",float(td['lon']))
-          
-    gmap.heatmap(lats,lons)
+
+    gmap.heatmap(lats, lons)
     #gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
     #gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
     #gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
     #gmap.heatmap(heat_lats, heat_lngs)
     gmap.draw(output_filename)
 
+if __name__ == '__main__':
+    main()
